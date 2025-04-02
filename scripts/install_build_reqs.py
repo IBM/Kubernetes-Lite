3a022e85b9a018fdb02d4e7c010a0755b442a053
Michael Honaker <Michael.Honaker@ibm.com>
2025-04-02 18:57:59 -0400

    Add support for zip in ci build
    Signed-off-by: Michael Honaker <Michael.Honaker@ibm.com>

--- scripts/install_build_reqs.py
+++ scripts/install_build_reqs.py
@@ -22,7 +22,7 @@ from urllib.request import urlopen
 
 # Base settings for go/src/dest
 GO_VERSION = "1.23.7"
-GO_BASE_URL = "https://go.dev/dl/go{version}.{system}-{arch}.tar.gz"
+GO_BASE_URL = "https://go.dev/dl/go{version!s}.{system!s}-{arch!s}.{archive!s}"
 DEST_PATH = tempfile.gettempdir()
 
 
@@ -44,7 +44,15 @@ class ArchTypes(StrEnum):
     S390 = "s390x"
 
 
+class ArchiveType(StrEnum):
+    """ArchiveType is a simple enum to track what type of archive go stores for a platform"""
+
+    ZIP = "zip"
+    TAR = "tar.gz"
+
+
 # Parse the system architecture and platform
+archive: ArchiveType | None = None
 system: SystemTypes | None = None
 arch: ArchTypes | None = None
 if platform.machine().lower() in {"x86_64", "amd64"}:
@@ -60,26 +68,34 @@ else:
 
 if platform.system() == "Linux":
     system = SystemTypes.LINUX
+    archive = ArchiveType.TAR
 elif platform.system() == "Darwin":
     system = SystemTypes.DARWIN
+    archive = ArchiveType.TAR
 elif platform.system() == "Windows":
     system = SystemTypes.WINDOWS
+    archive = ArchiveType.ZIP
 else:
     raise ValueError(f"Unknown machine system {platform.system()}")
 
 # Setup a directory to download the tar to
 with tempfile.TemporaryDirectory() as temp_dir:
     temp_path = Path(temp_dir)
-    tar_gz_file = temp_path / "go.tar.gz"
+    archive_file = temp_path / f"go.{archive!s}"
 
     # Template the url and send the request
-    go_url = GO_BASE_URL.format(version=GO_VERSION, arch=arch, system=system)
+    go_url = GO_BASE_URL.format(
+        version=GO_VERSION,
+        arch=arch,
+        system=system,
+        archive=archive,
+    )
     print(f"Attempting to download go src from: {go_url}", file=sys.stderr)
     with urlopen(go_url) as response:
-        tar_gz_file.write_bytes(response.read())
+        archive_file.write_bytes(response.read())
 
     print(f"Attempting to extract archive to: {DEST_PATH}", file=sys.stderr)
-    shutil.unpack_archive(tar_gz_file, DEST_PATH)
+    shutil.unpack_archive(archive_file, DEST_PATH)
 
 # Ensure go is usable. ! Note this requires the path be set externally
 subprocess.run([f"{DEST_PATH}/go/bin/go", "version"], check=True, capture_output=True)
